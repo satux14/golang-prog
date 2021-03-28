dbc15b2c9ff1ff7e6eae3957a872d6e940f565bb
Sathish <sathishkumarmnm@gmail.com>
2021-03-28 19:07:34 +0530

    basic golang

---
+++ cards/main.go
@@ -0,0 +1,36 @@
+package main
+
+var glob string
+var decSize int
+
+func main() {
+	// var card string = "Ace of Spades"
+	// var card = "Ace of Spades"
+	//card := "Ace of Spades" // := new variable
+	//card := newCard()
+	//fmt.Println(card)
+
+	//decSize = 52
+	//fmt.Println(decSize)
+
+	// card := []string{newCard(), newCard()}
+	//cards := deck{"Ace of Diamonds", newCard()}
+	//cards = append(cards, "Six of Spades")
+	//cards.print()
+	//deck.print(cards) -> this also possible. file.func(arg of same type)
+
+	/*
+		for i, card := range cards {
+			fmt.Println(i, card)
+		}
+	*/
+
+	//fmt.Println(cards)
+
+}
+
+/*
+func newCard() string {
+	return "Five of Diamonds"
+}
+*/
